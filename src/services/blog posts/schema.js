7e38666acad2c35a1594c03e6a0956d094a238d1
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-11-09 15:26:49 +0000

    New node js project
    Adds error Handlers and blog posts CRUD

---
+++ src/services/blog posts/schema.js
@@ -0,0 +1,43 @@
+import express from "express"
+
+const blogPostsRouter = express.Router()
+
+blogPostsRouter.post("/", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+blogPostsRouter.get("/", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+blogPostsRouter.get("/:postId", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+blogPostsRouter.put("/:postId", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+blogPostsRouter.delete("/:postId", async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
